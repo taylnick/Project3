b7266c875bccc8362d4a2a98c1305a652154e1cb
taylnick <taylnick@isu.edu>
2020-03-07 17:43:49 -0700

    Binary heap works!!!

--- proj3/NetworkRoutingSolver.py
+++ proj3/NetworkRoutingSolver.py
@@ -2,20 +2,26 @@
 
 
 from proj3.CS4412Graph import *
-from proj3.arrays import *
+from proj3.arrays import binary_heap, unsorted_array
 import time
 
 
 class NetworkRoutingSolver:
-    def __init__( self):
+
+    def __init__(self):
+        inf = float("inf")
         pass
 
-    def initializeNetwork( self, network ):
-        assert( type(network) == CS4412Graph )
+    def initializeNetwork(self, network):
+        assert (type(network) == CS4412Graph)
         self.network = network
+        nodes = self.network.nodes
+        self.dist = [float("inf")] * len(nodes)
+        self.prev = [None] * len(nodes)
 
-# given a destination, look at the distance structure and reconstruct the path from the source to the given destination.
-    def getShortestPath( self, destIndex ):
+    # given a destination, look at the distance structure and reconstruct the path from the source to the given
+    # destination.
+    def getShortestPath(self, destIndex):
         self.dest = destIndex
         # TODO: RETURN THE SHORTEST PATH FOR destIndex
         #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
@@ -23,47 +29,58 @@ class NetworkRoutingSolver:
         #       NEED TO USE
         path_edges = []
         total_length = 0
-        node = self.network.nodes[self.source]
-        edges_left = 3
-        while edges_left > 0:
-            edge = node.neighbors[2]
-            path_edges.append( (edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)) )
-            total_length += edge.length
-            node = edge.dest
-            edges_left -= 1
-        return {'cost':total_length, 'path':path_edges}
+        # node = self.network.nodes[self.source]
+        # edges_left = 3
+        # while edges_left > 0:
+        #     edge = node.neighbors[2]
+        #     path_edges.append((edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)))
+        #     total_length += edge.length
+        #     node = edge.dest
+        #     edges_left -= 1
+        # return {'cost': total_length, 'path': path_edges}
 
-    # This function is running Dijkstras. The result is a dist data structure that has the cost of the shortest path from every node.
-    # THat's why the source is given as a parameter.
-    # This does not actually create paths.
-    def computeShortestPaths( self, srcIndex, use_heap=False ):
-        self.source = srcIndex
+        path = []
+        src = self.prev[destIndex]
+        dest = destIndex
+        while src is not None:
+            for edge in self.network.nodes[src].neighbors:
+                if dest == edge.dest.node_id:
+                    path.insert(0, edge)
+                    dest = src
+                    src = self.prev[src]
+        for edge in path:
+            path_edges.append((edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)))
+            total_length += edge.length
+
+        return {'cost': total_length, 'path': path_edges}
+    # This function is running Dijkstras. The result is a dist data structure that has the cost of the shortest path
+    # from every node. THat's why the source is given as a parameter. This does not actually create paths.
+    def computeShortestPaths(self, srcIndex, use_heap=False):
+
         t1 = time.time()
         # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
         #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
         #       CALL TO getShortestPath(dest_index)
-        nodes = self.network.nodes
-        dist = [float("inf")] * len(nodes)
-
+        size = len(self.network.nodes)
         if use_heap:
-            pq = binary_heap(dist, srcIndex)
+            pq = binary_heap(self.dist, srcIndex)
         else:
-            pq = unsorted_array(dist, srcIndex)
+            pq = unsorted_array(self.dist, srcIndex)
 
-        prev = [None] * len(nodes)
-
+        self.dist[srcIndex] = 0
+        nodes = self.network.nodes
         while not pq.is_empty():
             curr = pq.delete_min()
-            for edge in curr.neighbors:
+            for edge in nodes[curr].neighbors:
                 next_id = edge.dest.node_id
-                curr_id = curr.node_id
-                new_weight = pq[curr_id] + edge.length
-                if pq[next_id] > new_weight:
-                    pq[next_id] = new_weight
-                    prev[next_id] = curr_id
+                new_weight = self.dist[curr] + edge.length
+
+                if not pq.still_contains(next_id):
+                    continue
+                if self.dist[next_id] > new_weight:
+                    self.dist[next_id] = new_weight
+                    self.prev[next_id] = curr
                     pq.decrease_key(next_id, new_weight)
 
-
         t2 = time.time()
-        return (t2-t1)
-
+        return t2 - t1
--- proj3/arrays.py
+++ proj3/arrays.py
@@ -1,44 +1,127 @@
-from proj3 import *
+from proj3.CS4412Graph import *
+from proj3.NetworkRoutingSolver import *
 
-class unsorted_array(list):
+
+class unsorted_array:
     def __init__(self, dist, src):
-        super().__init__()
         self.size = len(dist)
+        self.array = []
+        for i in range(self.size):
+            self.array.append(dist[i])
         self.decrease_key(src, 0)
 
     def is_empty(self):
         return self.size == 0
 
-    def insert(self, node, **kwargs):
+    def insert(self, node):
+        self.array.append(node)
+        self.size += 1
         pass
 
-# Returns node with min value freshly set to None
+    # Returns node with min value freshly set to None
     def delete_min(self):
-        id = None
-        min = float("inf")
-        for index, value in enumerate(self):
-            if (value is not None) and (value < min):
-                id = index
-                min = value
-        self.decrease_key(id, None)
-        return self[id]
+        min_id = None
+        min_value = float("inf")
+        for index, value in enumerate(self.array):
+            if (value is not None) and (value < min_value):
+                min_id = index
+                min_value = value
+        self.decrease_key(min_id, None)
+        self.size -= 1
+        return min_id
 
     def decrease_key(self, index, value):
-        self[index] = value
+        self.array[index] = value
+
 
 # Binary Heap class operations
 class binary_heap:
+
     def __init__(self, dist, src):
-        self.size = len(dist)
+        self.heapList = [0]
+        self.size = 0
+        self.point_arr = [-1] * len(dist)
+
+        for i in range(len(dist)):
+            self.insert(i, dist[i])
+        self.decrease_key(src, 0)
 
     def is_empty(self):
         return self.size == 0
 
-    def insert(self, node):
-        pass
+    def still_contains(self, i):
+        pos = self.point_arr[i]
+        if pos is None:
+            return False
+        else:
+            return pos <= self.size
 
+    def insert(self, i, val):
+        self.heapList.append(val)
+        self.size += 1
+        self.point_arr[i] = self.size
+        self.percolate_up(i)
+
+# Should return the index of the node removed.
     def delete_min(self):
-        pass
+        to_delete = self.point_arr.index(1)
+        node_id = self.point_arr.index(1)
+        self.point_arr[node_id] = None
+        if self.size == 1:
+            self.heapList.pop()
+            self.size -= 1
+            return to_delete
 
-    def decrease_key(self):
-        pass
+        self.heapList[1] = self.heapList[self.size]
+        new_top = self.point_arr.index(self.size)
+        self.point_arr[new_top] = 1
+        self.size -= 1
+        self.heapList.pop()
+        self.percolate_down(new_top)
+        return to_delete
+
+    def decrease_key(self, i, value):
+        pos = self.point_arr[i]
+        self.heapList[pos] = value
+        self.percolate_up(i)
+
+    def percolate_up(self, node_id):
+        pos = self.point_arr[node_id]
+        # while there is a parent
+        while pos // 2 > 0:
+            # if the child is weighted less than the parent
+            if self.heapList[pos] < self.heapList[pos // 2]:
+                # Swap weights in the tree
+                temp = self.heapList[pos // 2]
+                self.heapList[pos // 2] = self.heapList[pos]
+                self.heapList[pos] = temp
+                # Swap the position value of the two nodes.
+                swap_node = self.point_arr.index(pos // 2)
+                self.point_arr[node_id] = pos // 2
+                self.point_arr[swap_node] = pos
+            pos = pos // 2
+
+    def percolate_down(self, node_id):
+        pos = self.point_arr[node_id]
+        while (pos * 2) <= self.size:
+            min_pos = self.min_child(pos)
+            # if the parent is larger than its smallest child
+            if self.heapList[pos] > self.heapList[min_pos]:
+                # Swap the parent and child
+                temp = self.heapList[pos]
+                self.heapList[pos] = self.heapList[min_pos]
+                self.heapList[min_pos] = temp
+                # Swap the values in pointer_arr too
+                swap_node = self.point_arr.index(min_pos)
+                self.point_arr[node_id] = min_pos
+                self.point_arr[swap_node] = pos
+            pos = min_pos
+
+    def min_child(self, i):
+        if (i * 2 + 1) > self.size:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
